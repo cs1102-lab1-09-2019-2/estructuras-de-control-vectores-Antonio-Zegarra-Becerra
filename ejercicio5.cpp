06dfb788edde5af2db5190c1a6dcbd89f4019c95
Antonio-Zegarra-Becerra <54376590+Antonio-Zegarra-Becerra@users.noreply.github.com>
2019-08-30 21:18:43 -0500

    Ejercicio 6 Updated

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,28 +1,30 @@
-/*
- Dado una funcion cuadratica, donde los parametros a, b, y c son leidos por la consola.
- Calcular las soluciones basado en las siguientes reglas:
-
- Leer del usuario a, b, y c separados por espacio:
- cin>>a>>b>>c;
-
- 1.- Si a y b son cero, entonces: No tiene solucion: a==0, b ==0..
- 2.- Si a es cero, entonces: Tiene una sola solucion: (-c/b)
- 3.- Si b^2 -4ac es negativo, entonces: No tiene solucion: delta < 0..
- 4.- En el resto de los casos tiene dos soluciones y es calculado con la siguiente
- formula:
-
- x1 = -b + ((b^2 - 4ac)^(1/2))/2a
- x2 = -b - ((b^2 - 4ac)^(1/2))/2a
-
- La ecuacion tiene dos soluciones: x1 = x1, x2 = x2
-*/
-
 #include <iostream>
 #include <cmath>
 
 using namespace std;
-
+float delta(float,float,float);
 int main() {
-    float a, b, c, delta, x1, x2;
-
+    float a, b, c, x1, x2;
+    cout<<"Ingrese las variables a, b y c; separadas por un espacio: \n";
+    cin>>a>>b>>c;
+    if(a==0 && b==0){
+     cout<<"No tiene solución: \n";
+    }
+    else if(a==0){
+     cout<<"Solución: "<<(-c)/b;
+    }
+    else if(delta(a,b,c)<0){
+     cout<<"No tiene solución, delta<0: \n";
+    }
+    else{
+     x1 = -b + (sqrt(pow(b,2) - 4*a*c))/(2*a);
+     x2 = -b - (sqrt(pow(b,2) - 4*a*c))/(2*a);
+     cout<<"La ecuacion tiene dos soluciones: x1 = "<<x1<<", x2 = "<<x2;
+    }
+ 
 }
+
+float delta(float a,float b, float c){
+ return ((pow(b, 2) - 4*a*c)/abs((pow(b, 2) - 4*a*c)));
+}   
+            
