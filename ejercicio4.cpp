356918f3fdd26c2bd5cbff7fdca3b33f58b5cb00
Antonio-Zegarra-Becerra <54376590+Antonio-Zegarra-Becerra@users.noreply.github.com>
2019-08-30 20:34:19 -0500

    Ejercicio 4 Updated

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,20 +1,16 @@
-/*
-Escribir un programa que imprima lo siguiente:
 
-12*****
-123****
-1234***
-12345**
-123456*
-1234567
-
-*/
 
 #include <iostream>
 using std::cout;
 
 int main() {
-
-
+    int x=0,y=0,z=0;
+    for(y = 2; y <= 7; y++){
+        for(x = 1; x <= y; x++)
+            cout<<x;
+        for(z = 0; z <= 7-x; z++)
+            cout<<'*';
+        cout<<"\n";
+    }
     return 0;
 }
